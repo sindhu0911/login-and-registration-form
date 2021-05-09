bb3056ff4a7f66d3378b8ed42674dce18e1af9ee
sindhu0911 <sindhug608@gmail.com>
2021-05-09 20:46:17 +0530

    add project

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export class Footer extends Component {
+  render() {
+    return (
+      <footer>Copyright © 2021 sindhu, Inc. All Rights Reserved</footer>
+    )
+  }
+}
+
+export default Footer;
